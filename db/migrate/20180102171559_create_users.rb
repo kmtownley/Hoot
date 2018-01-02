d0a840e46874bd8a2944dc4fecd9daccaf24aed2
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-02 13:18:08 -0500

    Create user model

---
+++ db/migrate/20180102171559_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.boolean :business_owner
+      t.string :address 
+
+      t.timestamps
+    end
+    add_index :users, :name, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
