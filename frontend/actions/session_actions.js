a4ddc9dc8c8a13ed1910fdd328bc11c059c1877f
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-02 15:49:11 -0500

    Adds actions and reducers

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,27 @@
+import * as APIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const signup = user => dispatch => (
+  APIUtil.signup(user).then(user => dispatch(receiveCurrentUser(user)))
+);
+
+export const login = user => dispatch => (
+  APIUtil.login(user).then(user => dispatch(receiveCurrentUser(user)), err => dispatch(receiveErrors(err.response.JSON)))
+);
+
+export const logout = () => dispatch => (
+  APIUtil.logout().then(user => dispatch(receiveCurrentUser(null)))
+);
+
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIEVE_CURRENT_USER,
+  currentUser
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  currentUser
+});
