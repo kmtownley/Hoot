93a7465ada0d3ffcb40dc9d14d437885039f5415
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-02 17:16:14 -0500

    adds store

--- frontend/hoot.jsx
+++ frontend/hoot.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import configureStore from '.store/store';
-// import Root from './components/root';
+import configureStore from '.store/store';
+import Root from './components/root';
+
 import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
-  // const store = configureStore();
+  const store = configureStore();
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to Hoot</h1>, root);
   // window.dispatch = store.dispatch
-  window.$ = $
+  window.$ = $;
   window.signup = signup;
   window.login = login;
   window.logout = logout;
