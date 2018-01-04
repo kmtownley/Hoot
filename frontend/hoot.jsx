d7498cfce18e2d79bc058bd7797ddf9e9f0895c1
kmtownley <kathleen009@gmail.com>
2018-01-04 10:49:35 -0500

    Remover initialize in biz controller

--- frontend/hoot.jsx
+++ frontend/hoot.jsx
@@ -16,4 +16,6 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
+
+  window.$ = $; 
 });
