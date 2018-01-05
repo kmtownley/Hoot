56d4a7918934d60fdf1241c7098c79f8a5e9d763
kmtownley <kathleen009@gmail.com>
2018-01-05 18:16:10 -0500

    Updates nav

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -4,11 +4,23 @@ import AuthNavContainer from '../auth_navbar/auth_nav_container';
 
 class Navbar extends React.Component {
 
+  switchNavStyle() {
+    if (this.props.pathname === `/businesses/:businessId`) {
 
+      return (
+        <div className="top-red-bar">
+          <img src={window.staticImages.headerImage} />
+        </div>
+      )
+    }
+  }
 
   render() {
     return (
+      <main>
+        {this.switchNavStyle()}
       <AuthNavContainer />
+      </main>
     );
   }
 }
