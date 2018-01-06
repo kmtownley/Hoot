7a5900451b8dd2d3b041a8b05d6185ebe5e75574
kmtownley <kathleen009@gmail.com>
2018-01-06 10:55:39 -0500

    Creates reviews util

---
+++ frontend/util/review_api_util.js
@@ -0,0 +1,37 @@
+export const fetchReviews = (businessId) => (
+  $.ajax({
+    method: "GET",
+    url: `api/businesses/${businessId}/reviews`
+  })
+);
+
+export const fetchReview = id => (
+  $.ajax({
+    method: "GET",
+    url: `api/reviews/${id}`,
+  })
+);
+
+export const createReview = review => (
+  $.ajax({
+    method: "POST",
+    url: `api/businesses/${review.business_id}/reviews`,
+    data: { review }
+  })
+);
+
+export const updateReview = review => (
+  $.ajax({
+    method: "UPDATE",
+    url: `api/businesses/${review.business_id}/reviews/${review.id}`,
+    data: { review }
+  })
+);
+
+
+export const deleteReview = reviewId => (
+  $.ajax({
+    method: "DELETE",
+    url: `apu/reviews/${reviewId}`
+  })
+);
