446b3eaebe86d4d7d36c0e67ebb25deee4ecc424
kmtownley <kathleen009@gmail.com>
2018-01-05 23:21:24 -0500

    Adds appropriate login buttons

--- frontend/components/auth_navbar/auth_navbar.jsx
+++ frontend/components/auth_navbar/auth_navbar.jsx
@@ -5,6 +5,9 @@ import { Link } from 'react-router-dom';
 const AuthNavbar = (props) => {
   let navContents;
   let navClass;
+  const hootLogo = (<Link className="hoot-logo-container" to="/">
+    <img src={window.staticImages.headerImage} />
+  </Link>);
 
   if (props.location.pathname === '/') {
     navClass = "nav-landing";
@@ -18,27 +21,28 @@ const AuthNavbar = (props) => {
         className="header-logout"
         onClick={props.logout}>Logout
       </button>
-
     )
+  } else if (props.location.pathname === '/login' || props.location.pathname === '/signup') {
+    navContents = hootLogo
   } else {
     navContents = (
-      <div>
-        <nav className="login">
-          <Link to="/login"> Login </Link>
-        </nav>
-        <nav className="signup">
-          <Link to="/signup"> Sign Up </Link>
-        </nav>
-      </div>
+    <div>
+      <nav className="login">
+        <Link to="/login"> Login </Link>
+      </nav>
+      <nav className="signup">
+        <Link to="/signup"> Sign Up </Link>
+      </nav>
+    </div>
     )
   }
 
+  if (props.location.pathname !== '/') {
 
+  }
   return (
     <header className={navClass}>
-      <div>
-        {navContents}
-      </div>
+      {navContents}
     </header>
   )
 }
