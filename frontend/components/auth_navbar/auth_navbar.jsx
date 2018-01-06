e1359fb2540e2499b8112f5b0dfb3ec54b89b53a
kmtownley <kathleen009@gmail.com>
2018-01-06 10:10:48 -0500

    Updates navbar stying with hoot-logo

--- frontend/components/auth_navbar/auth_navbar.jsx
+++ frontend/components/auth_navbar/auth_navbar.jsx
@@ -5,25 +5,41 @@ import { Link } from 'react-router-dom';
 const AuthNavbar = (props) => {
   let navContents;
   let navClass;
-  const hootLogo = (<Link className="hoot-logo-container" to="/">
-    <img src={window.staticImages.headerImage} />
-  </Link>);
+  const hootLogo = (
+      <a className="hoot-logo" href="/" >
+        <img src={window.staticImages.headerImage} />
+      </a>
+  )
 
   if (props.location.pathname === '/') {
     navClass = "nav-landing";
+  } else if (props.location.pathname === '/businesess/:businessId') {
+    navClass = "nav-business-show"
   } else {
     navClass = 'nav-regular';
   }
 
-  if (props.currentUser){
+  if (props.currentUser && navClass !== "nav-landing"){
     navContents = (
-      <button
-        className="header-logout"
-        onClick={props.logout}>Logout
-      </button>
+      <div className="hoot-logo-container-left">
+        {hootLogo}
+        <button
+          className="header-logout"
+          onClick={props.logout}>Logout
+        </button>
+      </div>
     )
+  } else if (props.currentUser){
+    navContents = <button
+      className="header-logout"
+      onClick={props.logout}>Logout
+    </button>
   } else if (props.location.pathname === '/login' || props.location.pathname === '/signup') {
-    navContents = hootLogo
+    navContents = (
+      <div className="hoot-logo-container-center">
+        {hootLogo}
+      </div>
+    )
   } else {
     navContents = (
     <div>
