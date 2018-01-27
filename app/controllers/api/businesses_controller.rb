2f4d95d7327d66f8d8bc3dea94b7e57095b8c7a2
kmtownley <kathleen009@gmail.com>
2018-01-27 16:40:25 -0500

    Deletes comments

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -52,16 +52,16 @@ class Api::BusinessesController < ApplicationController
     else
       @businesses = Business.all
     end
-    # @businesses.each do |business|
-    #
-    # end
+    @businesses.each do |business|
+
+    end
 
   end
 
   private
 
   def business_params
-    params.require(:business).permit(:biz_name, :address, :city, :state, :zipcode, :phone_number, :latitude, :longitude, :price)
+    params.require(:business).permit(:biz_name, :address, :city, :state, :zipcode, :phone_number, :latitude, :longitude, :price, :mon_start_hr, :mon_end_hr, :tue_start_hr, :tud_end_hr,:wed_start_hr, :wed_end_hr,:thur_start_hr, :thur_end_hr,:fri_start_hr, :fri_end_hr,:sat_start_hr, :sat_end_hr, :sun_start_hr, :sun_end_hr, :delivery)
   end
 
   def bounds
